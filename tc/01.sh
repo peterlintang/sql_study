a25957e5b9d623eed1fcb173ddd96ae17048f366
linxiaojie <linxj@pcbaaa.com>
2026-09-17 10:49:40 +0800

    add del cmd

--- tc/01.sh
+++ tc/01.sh
@@ -9,4 +9,7 @@ sudo tc class add dev ens33 parent 1:1 classid 1:10 htb rate 10mbit ceil 100mbit
 sudo tc class add dev ens33 parent 1:1 classid 1:20 htb rate 10mbit ceil 100mbit prio 1
 #创建过滤器，挂在根队列下，端口80 tcp/udp数据包走1:20子类处理
 sudo tc filter add dev ens33 protocol ip parent 1:0 prio 1 u32 match ip dport 80 0xffff flowid 1:20
+#显示class详细信息
 #tc -s class show dev ens33
+#删除gen队列
+#sudo tc qdisc del dev ens33 root
